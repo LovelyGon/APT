2a324b42d4bb048d3a9edccce00d44084d6229b4
DELL <DELL@192.168.1.23>
2017-12-03 22:48:47 +0700

    test

--- resources/views/admin/basic_info.blade.php
+++ resources/views/admin/basic_info.blade.php
@@ -4,7 +4,7 @@
 <!-- SELECT2 EXAMPLE -->
 <div class="box box-default">
     <div class="box-header with-border">
-        <h3 class="box-title">Basic information</h3>
+        <h3 class="box-title">General</h3>
 
         <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
