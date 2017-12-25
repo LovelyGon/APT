7375bec83a98038304145c0c2d3477a8ef9ef823
DELL <DELL@192.168.1.13>
2017-12-25 22:46:56 +0700

    login-page

--- resources/views/admin/apartments-create.blade.php
+++ resources/views/admin/apartments-create.blade.php
@@ -1,5 +1,100 @@
 @extends('admin.admin_template') @section('content')
 
+<div class="box box-default">
+	<div class="box-header with-border">
+		<h3 class="box-title">General</h3>
 
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse">
+				<i class="fa fa-minus"></i>
+			</button>
+
+		</div>
+	</div>
+	<!-- /.box-header -->
+	<div class="box-body">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label>Apartment type</label> <select id="apartmentType"
+						class="form-control select2">
+						<option style="" selected="selected">N/A</option>
+						<option style="" selected="selected">1 ✯</option>
+						<option class="fa-star">2 ✯ ✯</option>
+						<option class="fa-star">3 ✯ ✯ ✯</option>
+						<option class="fa-star">4 ✯ ✯ ✯ ✯</option>
+						<option class="fa-star">5 ✯ ✯ ✯ ✯ ✯</option>
+					</select>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+
+				<div class="form-group">
+					<label>Apartment name</label>
+					<div class="alert alert-info alert-dismissible">You can choose one
+						of suggested name from the list. If there's no name like your
+						apartment. Please enter a custom name</div>
+					<select class="form-control select2">
+
+						<option selected="selected">Apartment</option>
+						<option>Monthly Rental Hotel</option>
+						<option>Shared house</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="form-group">
+					<label>Custom name</label>
+					<div class="alert alert-info alert-dismissible">Just enter a custom
+						name in case there's really nothing like your apartment from
+						Apartment name</div>
+					<input type="text" class="form-control" id="customName"
+						placeholder="Enter custom name">
+				</div>
+			</div>
+
+
+		</div>
+		<!-- /.row -->
+
+
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label>Number of apartment(of this type)</label> <input
+						id="numberApartment" type="number" class="form-control"></input>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<label>Smoking policy</label> <select class="form-control select2">
+
+					<option selected="selected" value="nonesmoking">Non-smoking</option>
+					<option value="smoking">Smoking</option>
+				</select>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-3">
+				<label>Number of bedrooms</label> 
+				<input id="bedroomNumber" type="number" min="1" class="form-control" onkeypress='return event.charCode >= 48 && event.charCode <= 57'></input>
+			</div>
+			<div class="col-md-3">
+				<label>Number of living rooms</label> 
+				<input id=livingroomNumber type="number" min="0" class="form-control" onkeypress='return event.charCode >= 48 && event.charCode <= 57'></input>
+			</div>
+			<div class="col-md-3">
+				<label>Number bathrooms</label> 
+				<input id=bathroomNumber type="number" min="0" class="form-control" onkeypress='return event.charCode >= 48 && event.charCode <= 57'></input>
+			</div>
+		</div>
+
+	</div>
+</div>
 
 @endsection
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SimpleAdminLTE 2 | Log in</title>
+  <title>Airmonda.com | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -27,13 +27,13 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index2.html"><b>Softsed </b>SimpleAdminLTE</a>
+    <a href="airmonda.com"><b>Airmonda.com </b>Apartment management</a>
   </div>
-  <!-- /.login-logo -->
+ <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="index2.html" method="post">
+    <form action="../../index2.html" method="post">
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -45,7 +45,9 @@
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
-
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
           </div>
         </div>
         <!-- /.col -->
@@ -56,10 +58,17 @@
       </div>
     </form>
 
-
+    <div class="social-auth-links text-center">
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+        Google+</a>
+    </div>
     <!-- /.social-auth-links -->
 
-   
+    <a href="#">I forgot my password</a><br>
+    <a href="register.html" class="text-center">Register a new membership</a>
 
   </div>
   <!-- /.login-box-body -->
