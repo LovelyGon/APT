3edd5a1d2ef9015e13921ac28d2c33ffa30c5462
DELL <DELL@192.168.1.13>
2017-12-25 14:38:33 +0700

    new route

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,46 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | contains the "web" middleware group. Now create something great!
+ * |
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(array('prefix' => 'admin'), function () {
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    Route::get('home', function () {
+        return view('admin.home');
+    });
+    
+    Route::get('basicinfo', function () {
+        return view('admin.basic_info');
+    });
+    
+    Route::get('features', function () {
+        return view('admin.features');
+    });
+    
+    Route::get('apartments/create', function () {
+        return view('admin.apartments-create');
+    });
+    
+    Route::get('apartments', function () {
+        return view('admin.apartments');
+    });
+    
+    Route::get('admin/home', 'BasicInfoController@index');
+    
+    Route::get('admin/login', function () {
+        return view('admin.login');
+    });
 });
-
-Route::get('admin/home', function () {
-    return view('admin.home');
-});
-
-Route::get('admin/basicinfo', function () {
-    return view('admin.basic_info');
-});
-
-Route::get('admin/home', 'BasicInfoController@index');
-
-Route::get('admin/login', function () {
-    return view('admin.login');
-});
-
