1bf0e4af969990a4d9690b89e22a048c2ef7bb65
DELL <DELL@192.168.1.4>
2018-01-02 15:39:36 +0700

    appy multi-lang

---
+++ resources/lang/en/property.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Property Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Apartment function
+    | You are free to change them to anything you want to customize your views to 
+    | better match your application.
+    |
+    */
+
+    'general_info' => 'General information translated',
+    'next' => 'Next &raquo;',
+
+];
