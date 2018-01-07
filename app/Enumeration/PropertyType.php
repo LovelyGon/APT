34b827a47328f31f52f82fd3040d9572fb5462c3
DELL <DELL@192.168.1.8>
2018-01-07 12:00:00 +0700

    PropertyType enum

---
+++ app/Enumeration/PropertyType.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Enumeration;
+
+use MyCLabs\Enum\Enum;
+
+class PropertyType extends Enum
+{
+
+    const apt = 'apt';
+
+    const mhr = 'mhr';
+
+    const ssh = 'ssh';
+
+    public static function toArray()
+    {
+        $translatedItem = array();
+        
+        $class = get_called_class();        
+        $reflection = new \ReflectionClass($class);
+        $items = $reflection->getConstants();
+        
+        foreach ($items as $key => $value) {
+            $translatedItem[$key] = __('property.property_type.' . $items[$key]);
+        }
+        
+        return $translatedItem;
+    }
+}
