88e72a74251e320d5da2c30f85dcca3932d899fa
DELL <DELL@192.168.1.2>
2018-01-12 10:45:07 +0700

    remove some stuffs

--- resources/views/admin/admin_template.blade.php
+++ resources/views/admin/admin_template.blade.php
@@ -75,7 +75,7 @@
 <script src="{{asset('js/bootstrap-filestyle.min.js')}}"></script>
 <script src="{{asset('js/countrySelect.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('dist/js/demo.js')}}"></script>\
+<script src="{{asset('dist/js/demo.js')}}"></script>
   
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
