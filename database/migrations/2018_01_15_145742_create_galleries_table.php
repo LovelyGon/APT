bfb3698174a2d323950aa948a2c716fcd418d481
DELL <DELL@192.168.1.18>
2018-01-15 22:01:30 +0700

    new table:property_galleries and galleries(for apartment)

---
+++ database/migrations/2018_01_15_145742_create_galleries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->String('image_name')->nullable();
+            $table->integer('apartment_id')->unsigned()->nullable();
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
